c62e68b82114edb56bf27cf206bc74f416643cf0
Roshan kumar Jha <jharoshankumar850@gmail.com>
2025-11-20 15:44:40 +0530

    add
    add

---
+++ frontend/signup/database/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/signupDB';
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
