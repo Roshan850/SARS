5ad21373b4ec52199b53dd9768c45712d5d80eff
Roshan kumar Jha <jharoshankumar850@gmail.com>
2025-11-21 07:32:44 +0530

    add
    add

--- BackEnd/src/config/database.js
+++ BackEnd/src/config/database.js
@@ -1,26 +1,15 @@
-const mongoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
+const mogooser=require("mongoose");
+const env=require('dotenv').config();
 
-<<<<<<< HEAD
-const DB_URL=process.env.CONNECTION_STRING;
+const DB_URL=process.env.DB_URL;
 mongooes.connect("mongodb://127.0.0.1:27017/SARS",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     }).tehn(()=>{
         console.log("Database connected Sucessfully");
-=======
-const connectDB = () => {
-    return mongoose
-        .connect("mongodb://127.0.0.1:27017/SARS", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("Database connected Successfully");
-        })
-        .catch((error) => {
-            console.log("Database connection failed", error);
-        });
-};
->>>>>>> 1919bac9c0db0986a00b168dd062a4b85e5b5ffd
 
-module.exports = connectDB;
+    }).catch((error)=>{
+        console.log("Database connection failed".error);
+    });
+    module.exports=mongoose;
